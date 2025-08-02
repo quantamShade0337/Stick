275667ad6bbb137446ae57be1952d1c797d6cace
Ethan Soh <ethan.sohyt@gmail.com>
2025-08-02 09:53:53 +0800

    Initial Commit

---
+++ Stick/StickApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickApp.swift
+//  Stick
+//
+//  Created by Ethan Soh on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
